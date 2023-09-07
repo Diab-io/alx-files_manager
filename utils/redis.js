4ad3058a92759078f48761eddadac5a7dadbec7c
Odimayo David <odimayodavid7@gmail.com>
2023-09-07 13:39:17 +0100

    Base files

---
+++ utils/redis.js
@@ -0,0 +1,35 @@
+import { promisify } from 'util';
+import { createClient } from "redis";
+
+class RedisClient {
+    constructor() {
+        this.client = createClient().on('error', (err) => {
+            console.log(err)
+            this.isConnected = false
+        });
+        this.isConnected = true
+        this.client.on('connect', () => {
+            this.isConnected = true
+        })
+
+    }
+
+    isAlive() {
+        return this.isConnected
+    }
+
+    async get(key) {
+        return await promisify(this.client.get).bind(this.client)(key);
+    }
+
+    async set(key, value, duration) {
+        await promisify(this.client.setex).bind(this.client)(key, duration, value);
+    }
+
+    async del(key) {
+        await promisify(this.client.del).bind(this.client)(key)
+    }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
