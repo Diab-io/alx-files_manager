296dd826073687c49a016912e68506f2a08e0444
Odimayo David <odimayodavid7@gmail.com>
2023-09-09 11:20:45 +0100

    Module that coonnects to the mongdb database

--- utils/db.js
+++ utils/db.js
@@ -1,36 +1,36 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 class DBClient {
-    constructor() {
-        this.host = process.env.DB_HOST || 'localhost'
-        this.port = process.env.DB_PORT || 27017
-        this.database = process.env.DB_DATABASE || 'files_manager'
-        const uri = `mongodb://${this.host}:${this.port}/${this.database}`
-        this.client = new MongoClient(uri, { useUnifiedTopology: true })
-        this.client.connect()
-    }
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${this.host}:${this.port}/${this.database}`;
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
+    this.client.connect();
+  }
 
-    isAlive() {
-        return this.client.isConnected()
-    }
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-    async nbUsers() {
-        const usersCollection = this.getCollection('users')
-        const documentCount = await usersCollection.find().count()
-        return documentCount;
-    }
+  async nbUsers() {
+    const usersCollection = this.getCollection('users');
+    const documentCount = await usersCollection.find().count();
+    return documentCount;
+  }
 
-    async nbFiles() {
-        const collection = this.getCollection('files')
-        const documentCount = await collection.find().count()
-        return documentCount
-    }
+  async nbFiles() {
+    const collection = this.getCollection('files');
+    const documentCount = await collection.find().count();
+    return documentCount;
+  }
 
-    getCollection(collectionName) {
-        const db = this.client.db()
-        const collection = db.collection(collectionName)
-        return collection
-    }
+  getCollection(collectionName) {
+    const db = this.client.db();
+    const collection = db.collection(collectionName);
+    return collection;
+  }
 }
 
 const dbClient = new DBClient();
