8ec9345e349b1a19cedb00b86e9c828d7e0e48a1
Odimayo David <odimayodavid7@gmail.com>
2023-09-09 12:04:30 +0100

    Created file that acts as the server

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-import express from "express";
-import router from "./routes/index"
+import express from 'express';
+import router from './routes/index';
 
-
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json())
-app.use('/', router)
+app.use(express.json());
+app.use('/', router);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
 
 export default app;
