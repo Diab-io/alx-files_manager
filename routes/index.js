4b656a900693948dd82e2f73983c5efb443bbe80
Odimayo David <odimayodavid7@gmail.com>
2023-09-18 16:17:10 +0100

    Added the post files route

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,19 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
+// get routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+
+// post routes
 router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
 
 export default router;
